e4b75d5d2df6e0567e29fe113e8f1d065385be05
Egor Rya <fayncore@gmail.com>
2020-09-06 21:43:52 +0300

    challenge part 2

--- app.js
+++ app.js
@@ -50,8 +50,17 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
         // update the ui
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+        let input = document.querySelector(".final-score").value
+        let winningScore;
+
+        if (input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
+
         // check if player win the game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
